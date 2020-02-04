3c56788ba0b3ef04758a83437b182cf77e3cdd2b
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-02-04 17:27:56 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,40 +1,16 @@
-class Transfer
-  attr_reader :sender, :receiver, :amount
-  attr_accessor :status
-
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
+class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
   end
-
-  def valid?
-    sender.valid? && receiver.valid?
+ 
+  def self.all
+    @@all
   end
-
-  def execute_transaction
-
-    if self.valid? && self.status == "pending" && amount <= sender.balance
-      sender.balance -= amount
-      receiver.balance += amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-
-    end
-
-  end
-
-  def reverse_transfer
-    if self.status == "complete"
-      sender.balance += amount
-      receiver.balance -= amount
-      self.status = "reversed"
-    end
-
-  end
-
-
+ 
 end
